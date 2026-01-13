6a15c1849d6dcc03ffb0c416d98c34afa6e0d682
Nirali Somia <niralisomia@Niralis-MacBook-Pro.local>
2026-01-13 00:54:26 -0500

    backgrounds

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,7 @@
 const themeMap = {
   'default-light': {
     'background-color': 'white',
-    'background-image': 'test.jpg',  // Store just the filename
+    'background-image': 'Back_DefLight.jpg',  // Store just the filename
     'background-size': 'cover',        // or 'contain', '100% 100%', etc.
     'background-position': 'center',   // or 'top', 'bottom', etc.
     'background-repeat': 'no-repeat',  // or 'repeat', 'repeat-x', etc.
@@ -11,16 +11,31 @@ const themeMap = {
   },
   'default-dark': {
     'background-color': '#222',
+    'background-image': 'Back_DefDark.jpg',  // Store just the filename
+    'background-size': 'cover',        // or 'contain', '100% 100%', etc.
+    'background-position': 'center',   // or 'top', 'bottom', etc.
+    'background-repeat': 'no-repeat',  // or 'repeat', 'repeat-x', etc.
+    'background-attachment': 'fixed',  // or 'scroll'
     'text-color': 'white',
     'highlight-color': '#2e2e2e'
   },
   'solarized-light': {
     'background-color': '#fdf6e3',
+    'background-image': 'Back_SolLight.jpg',  // Store just the filename
+    'background-size': 'cover',        // or 'contain', '100% 100%', etc.
+    'background-position': 'center',   // or 'top', 'bottom', etc.
+    'background-repeat': 'no-repeat',  // or 'repeat', 'repeat-x', etc.
+    'background-attachment': 'fixed',  // or 'scroll'
     'text-color': '#657b83',
     'highlight-color': '#eee8d5'
   },
   'solarized-dark': {
     'background-color': '#002b36',
+    'background-image': 'Back_SolDark.jpg',  // Store just the filename
+    'background-size': 'cover',        // or 'contain', '100% 100%', etc.
+    'background-position': 'center',   // or 'top', 'bottom', etc.
+    'background-repeat': 'no-repeat',  // or 'repeat', 'repeat-x', etc.
+    'background-attachment': 'fixed',  // or 'scroll'
     'text-color': '#839496',
     'highlight-color': '#073642'
   },
@@ -36,6 +51,11 @@ const themeMap = {
   },
   'sepia-dark': {
     'background-color': '#3e2c1c',
+    'background-image': 'Back_SepiaDark.jpg',  // Store just the filename
+    'background-size': 'cover',        // or 'contain', '100% 100%', etc.
+    'background-position': 'center',   // or 'top', 'bottom', etc.
+    'background-repeat': 'no-repeat',  // or 'repeat', 'repeat-x', etc.
+    'background-attachment': 'fixed',  // or 'scroll'
     'text-color': '#d8c3a5',
     'highlight-color': '#4b382a'
   }
